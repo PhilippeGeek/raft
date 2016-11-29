5078ff5e8180e76907fe7f39a4efd8ec5dc42c12
Philippe Vienne <philippegeek@gmail.com>
2016-11-29 08:47:55 +0100

    Store nodes as list

--- src/main/java/fr/insalyon/tc/raft/Node.java
+++ src/main/java/fr/insalyon/tc/raft/Node.java
@@ -1,11 +1,26 @@
 package fr.insalyon.tc.raft;
 
+import java.util.ArrayList;
+
 /**
  * Represent a node of the system.
  */
 public class Node {
 
+    /**
+     * Current state of this node.
+     */
     private State state;
+
+    /**
+     * All linked nodes
+     */
+    private ArrayList<Node> otherNodes = new ArrayList<>();
+
+    /**
+     * The current leader node in the network
+     */
+    private Node leader;
 
     public Node(){
         state = State.FOLLOWER;
